f19d5c8099af687b260cf13cb0967f6576686b83
league <league@imac-14.attlocal.net>
2022-11-11 16:54:04 -0800

    We made riddles and moved rocket ships

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,7 +1,10 @@
 PImage pepperoni;
 PImage mushroom;
+PImage olive;
 void setup() {
     size(600,600);
+  
+   
 }
 void draw() {
   fill(196, 164, 132);
@@ -12,19 +15,27 @@ void draw() {
   ellipse(300,300,450,450);
   pepperoni = loadImage("pepperoni.png");
   mushroom = loadImage("mushroom.png");
+  olive = loadImage("olive.png");
+  olive.resize(27,27);
   image(pepperoni,200,200);
   image(pepperoni,300,400);
   image(pepperoni,195,425);
   image(pepperoni,350,150);
   image(pepperoni,150,298);
   image(pepperoni,378,200);
-  image(pepperoni,230,450);
-  image(mushroom,275,175);
-  image(mushroom,435,175);
-  image(mushroom,234,234);
-  image(mushroom,143,453);
-  image(mushroom,345,175);
-  image(mushroom,123,456);
-  image(mushroom,456,123);
-  
+  image(pepperoni,256,298);
+  image(mushroom,407,357);
+  image(mushroom,129,129);
+  image(mushroom,145,231);
+  image(mushroom,345,378);
+  image(mushroom,385,114);
+  image(mushroom,200, 445);
+  image(mushroom,109,395);
+  image(olive,100,300);
+  image(olive,300,100);
+  image(olive,450,300);
+  image(olive,337,310);
+  image(olive,250,450);
+  image(olive,279,196);
+  image(olive,430,440);
 }
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,16 +12,38 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+		int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+		String question1 = JOptionPane.showInputDialog(null,"What starts with a f. ends with an e, and isn't asian? (ALL CAPS)");
+		// 4. If they got the answer right, pop up "correct!" and increase the score by 
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+		if (question1.equals("FAILIURE")) {
+			JOptionPane.showMessageDialog(null, "Decent");
+			score+=1;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "FAILIURE (like you)");
+		}
 		// 6. Add some more riddles
-
+		String question2 = JOptionPane.showInputDialog(null,"Who is really cool and handsome and awesome? (asian) (ALL CAPS)");
 		// 2. Make a pop up to show the score.
+		if (question2.equals("BRIAN")) {
+			JOptionPane.showMessageDialog(null, "You are truly so wise");
+			score+=1;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "FAILIURE (I'm going to send sensei steven to your house)");
+			
+		}
+		String question3 = JOptionPane.showInputDialog(null,"what starts with an e and ends with an e? (ALL CAPS) (PUT SPACE BETWEEN 2 WORDS)");
+		
+		if (question3.equals("EMOTIONAL DAMAGE")) {
+			JOptionPane.showMessageDialog(null, "Clearly you have suffered deeply");
+			score+=1;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "sensei steven will be dissapointed");
+		}
 		
 	}
 }
